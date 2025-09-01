009d219486139f511354349ad4a4b573262114e0
adeebatgithub <adeebdanish063@gmail.com>
2025-09-01 15:43:12 +0530

    patch 1.0.2 - basic accounting

--- DjangoAccounting/settings.py
+++ DjangoAccounting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-17oemo5!6%^l7b&w%atx9gso#bxk-o%=-enrk+vwnsqb96frq&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
