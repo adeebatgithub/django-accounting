9735551ac0892934790084c473451ec7bb268779
adeebatgithub <adeebdanish063@gmail.com>
2025-09-02 18:52:51 +0530

    patch 1.0.0 - basic accounting

--- DjangoAccounting/settings.py
+++ DjangoAccounting/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-17oemo5!6%^l7b&w%atx9gso#bxk-o%=-enrk+vwnsqb96frq&
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -92,17 +90,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DjangoAccounting.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "adeeb_render_db",
+        "USER": "adeeb_render_db_user",
+        "PASSWORD": "T5GXDbboVu0EjoteHI1TJekMROJkB3sl",
+        "HOST": "dpg-d2reio56ubrc73ejee3g-a.singapore-postgres.render.com",
+        "PORT": "5432",  # default Postgres port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -122,7 +122,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -133,7 +132,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
