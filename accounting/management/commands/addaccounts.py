d717f90a4709bda8e6eb4edebc3010b1b40ee6e3
adeebatgithub <adeebdanish063@gmail.com>
2025-09-06 08:29:23 +0530

    patch 1.0.0.1 - basic accounting

--- accounting/management/commands/addaccounts.py
+++ accounting/management/commands/addaccounts.py
@@ -4,7 +4,14 @@ from accounting.models import AccountModel
 from accounting.etc.accounts import LIST_OF_ACCOUNTS
 
 class Command(BaseCommand):
+    def _account_exists(self, account_name):
+        return AccountModel.objects.filter(name=account_name).exists()
+
     def create_account(self, **kwargs):
+        if self._account_exists(kwargs['name']):
+            self.stdout.write(f'Account Exists: {kwargs['name']}')
+            return
+
         obj = AccountModel.objects.create(**kwargs)
         self.stdout.write(f'Account created: {obj.name} under > {obj.parent}')
 
