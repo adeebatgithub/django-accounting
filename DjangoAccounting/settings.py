e210944d3fde55f175e951c742402d23eecd2659
adeebatgithub <adeebdanish063@gmail.com>
2025-09-07 12:03:53 +0530

    patch 1.0.0.2 - basic accounting

--- DjangoAccounting/settings.py
+++ DjangoAccounting/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-17oemo5!6%^l7b&w%atx9gso#bxk-o%=-enrk+vwnsqb96frq&'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+API_BASE_URL = env.str("API_BASE_URL")
 
 # Application definition
 
@@ -96,10 +100,7 @@ WSGI_APPLICATION = 'DjangoAccounting.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db("DATABASE")
 }
 
 
